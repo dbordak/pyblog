61ccf2d2b737faada53f1e4aaf2d9b0f9771b54c
Daniel Bordak <dbordak@gmail.com>
2014-05-18 22:12:54 -0400

    Initial commit

---
+++ blargh.py
@@ -0,0 +1,14 @@
+"""What will eventually be a blog."""
+
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
