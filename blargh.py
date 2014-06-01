f486cde5df33ecd100ce102596fa3093a8405dfe
Daniel Bordak <dbordak@gmail.com>
2014-06-01 11:29:18 -0400

    Refactoring

--- blargh.py
+++ blargh.py
@@ -16,37 +16,28 @@ def genSidebar(blog, user=None):
     line = "This is a blog."
     if user and users.is_current_user_admin():
         line += '<br><a href="/admin/">Admin Page</a>'
-    return line
+    return {'sidebar': line}
 
 
 class MainPage(webapp2.RequestHandler):
     """List of blog entry summaries."""
     def get(self):
         blog = self.request.get('blog', 'blog')
-        sidebar = genSidebar(blog, users.get_current_user())
+        template_values = genSidebar(blog, users.get_current_user())
         entry_query = Entry.query(ancestor=entry_key(blog)).order(-Entry.date)
 
-        template_values = {
-            'entries': entry_query.fetch(10),
-            'sidebar': sidebar
-        }
+        template_values['entries'] = entry_query.fetch(10)
 
         template = JINJA_ENVIRONMENT.get_template('templates/index.html')
         self.response.write(template.render(template_values))
 
 
-class Manage(webapp2.RequestHandler):
+class AdminPage(webapp2.RequestHandler):
     """Administration panel for (adding? modifying?) entries"""
     def get(self):
-        sidebar = genSidebar(self.request.get('blog', 'blog'))
-        template_values = {
-            'heading': "Administation",
-            'body': "This is the administration page. Post a post with a POST"
-            + " to this URL to post that post onto the blog, posthaste!",
-            'sidebar': sidebar
-        }
+        template_values = genSidebar(self.request.get('blog', 'blog'))
 
-        template = JINJA_ENVIRONMENT.get_template('templates/layout.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/admin.html')
         self.response.write(template.render(template_values))
 
     def post(self):
@@ -61,18 +52,13 @@ class Manage(webapp2.RequestHandler):
         self.redirect('/?' + urllib.urlencode(query_params))
 
 
-class About(webapp2.RequestHandler):
+class AboutPage(webapp2.RequestHandler):
     """About me page"""
     def get(self):
-        sidebar = genSidebar(self.request.get('blog', 'blog'),
-                             users.get_current_user())
-        template_values = {
-            'heading': "About",
-            'body': "TODO",
-            'sidebar': sidebar
-        }
+        template_values = genSidebar(self.request.get('blog', 'blog'),
+                                     users.get_current_user())
 
-        template = JINJA_ENVIRONMENT.get_template('templates/layout.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/about.html')
         self.response.write(template.render(template_values))
 
 
@@ -81,12 +67,14 @@ class Entry(ndb.Model):
     title = ndb.StringProperty(indexed=False, required=True)
     content = ndb.TextProperty(required=True)
     date = ndb.DateTimeProperty(auto_now_add=True, required=True)
-    tags = ndb.StringProperty(repeated=True)
-    url = ndb.ComputedProperty(
-        lambda self:
-        "post/" + str(self.date.year) + "/" + str(self.date.month) + "/" +
-        str(self.date.day) + "/" + self.title.lower()
-    )
+    category = ndb.StringProperty(required=True)
+    subcat = ndb.StringProperty(required=True)
+    # tags = ndb.StringProperty(repeated=True)
+
+    @property
+    def url(self):
+        return urllib.quote_plus(self.category + "/" + self.subcat + "/" +
+                                 self.title)
 
 
 def entry_key(blog_name='blog'):
@@ -95,7 +83,7 @@ def entry_key(blog_name='blog'):
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/admin', Manage)
-    #('/post/', BlogPost)
-    #('/about', About)
+    ('/admin', AdminPage)
+    #('/post/', EntryPage)
+    #('/about', AboutPage)
 ], debug=True)
