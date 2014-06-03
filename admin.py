3b48e1bf5f170d993552bbb97e32a2f8db3026ee
Daniel Bordak <dbordak@gmail.com>
2014-06-03 15:01:04 -0400

    Added Category deletion, fixed dropdown menus

--- admin.py
+++ admin.py
@@ -1,18 +1,13 @@
 """All administration pages."""
 
-from os.path import dirname
 from urllib import urlencode
 from google.appengine.ext import ndb
 import webapp2
-import jinja2
 import models
-from blargh import handle404
-
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(dirname(__file__)))
+from blargh import handle404, JINJA_ENVIRONMENT
 
 class AddCategoryPage(webapp2.RequestHandler):
-    """Administration panel for adding entries"""
+    """Administration panel for adding categories"""
     def get(self):
         template_values = {'cats': models.Category.query()}
         template = JINJA_ENVIRONMENT.get_template('templates/admin/add_cat.html')
@@ -22,16 +17,32 @@ class AddCategoryPage(webapp2.RequestHandler):
         req = self.request
         newCategory = models.Category()
 
-        newCategory.name   = req.get('name')
-        try:
-            newCategory.parent = ndb.Key(urlsafe=req.get('parent'))
-        except:
-            pass
+        newCategory.name = req.get('name')
+        par = req.get('parent')
+        if par != "":
+            newCategory.parent = ndb.Key(urlsafe=par)
         newCategory.put()
 
         query_params = {'blog': req.get('blog', 'blog')}
         self.redirect('/?' + urlencode(query_params))
 
+# TODO: CSRF, Wap, rewrite with Kay
+class DeleteCategoryPage(webapp2.RequestHandler):
+    """Administration panel for deleting categories"""
+    def get(self):
+        template_values = {'cats': models.Category.query()}
+        template = JINJA_ENVIRONMENT.get_template('templates/admin/del_cat.html')
+        self.response.write(template.render(template_values))
+
+    def post(self):
+        req = self.request
+
+        cat = req.get('category')
+        if cat != "":
+            ndb.Key(urlsafe=cat).delete()
+
+        query_params = {'blog': req.get('blog', 'blog')}
+        self.redirect('/?' + urlencode(query_params))
 
 class AddEntryPage(webapp2.RequestHandler):
     """Administration panel for adding entries"""
@@ -44,16 +55,18 @@ class AddEntryPage(webapp2.RequestHandler):
         req = self.request
         newEntry = models.Entry()
 
-        newEntry.title    = req.get('title')
-        newEntry.content  = req.get('content')
-        newEntry.category = ndb.Key(urlsafe=req.get('category'))
+        newEntry.title   = req.get('title')
+        newEntry.content = req.get('content')
+        cat = req.get('category')
+        if cat != "":
+            newEntry.category = ndb.Key(urlsafe=cat)
         newEntry.put()
 
         query_params = {'blog': req.get('blog', 'blog')}
         self.redirect('/?' + urlencode(query_params))
 
 
-class AdminNavPage(webapp2.RequestHandler):
+class NavPage(webapp2.RequestHandler):
     """Navigation page for administrative tasks."""
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('templates/admin/nav.html')
@@ -61,9 +74,10 @@ class AdminNavPage(webapp2.RequestHandler):
 
 
 application = webapp2.WSGIApplication([
-    ('/admin/', AdminNavPage),
+    ('/admin/', NavPage),
     ('/admin/add/ent', AddEntryPage),
     ('/admin/add/cat', AddCategoryPage),
+    ('/admin/del/cat', DeleteCategoryPage),
 ], debug=True)
 
 application.error_handlers[404] = handle404
