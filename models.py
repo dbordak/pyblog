c6e9f00eee158e643295fd1764f8c7a3768616dc
Daniel Bordak <dbordak@gmail.com>
2014-06-03 09:17:29 -0400

    Remodelling

--- models.py
+++ models.py
@@ -2,14 +2,24 @@ from urllib import quote_plus
 from google.appengine.ext import ndb
 
 
+class Category(ndb.Model):
+    name = ndb.StringProperty('n', required=True)
+    entries = ndb.StructuredProperty(Entry, 'e', repeated=True)
+
+
+class SubCategory(ndb.Model):
+    name = ndb.StringProperty('n', required=True)
+    entries = ndb.KeyProperty('e', kind=Entry, repeated=True)
+    parent = ndb.KeyProperty('p', kind=Category)
+
+
 class Entry(ndb.Model):
     """Models a blog entry"""
     title = ndb.StringProperty('t', required=True)
     content = ndb.TextProperty('c', required=True)
     date = ndb.DateTimeProperty('d', auto_now_add=True, required=True)
-    category = ndb.StringProperty('cat', required=True)
-    subcat = ndb.StringProperty('sc', required=True)
-    # tags = ndb.StringProperty(repeated=True)
+    category = ndb.KeyProperty('cat', kind=Category)
+    subcat = ndb.KeyProperty('sc', kind=SubCategory)
 
     @property
     def url(self):
